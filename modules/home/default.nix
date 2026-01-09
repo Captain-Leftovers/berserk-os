eb0b53e6df933f20ba3d79751bad4c35eaeb0ddf
beeondweb <beeondweb@gmail.com>
2026-01-10 09:00:36 +1100

    adding qflipper'
    Signed-off-by: beeondweb <beeondweb@gmail.com>

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -1,6 +1,6 @@
-{ host, ... }:
-let
-  inherit (import ../../hosts/${host}/variables.nix)
+{host, ...}: let
+  inherit
+    (import ../../hosts/${host}/variables.nix)
     alacrittyEnable
     ghosttyEnable
     tmuxEnable
@@ -10,65 +10,89 @@ let
     helixEnable
     doomEmacsEnable
     ;
-in
-{
-  imports = [
-    ./amfora.nix
-    ./bash.nix
-    ./bashrc-personal.nix
-    ./bat.nix
-    ./btop.nix
-    ./bottom.nix
-    ./cava.nix
-    ./emoji.nix
-    ./expressvpn.nix
-    ./eza.nix
-    ./fastfetch
-    ./fzf.nix
-    ./gh.nix
-    ./git.nix
-    ./gtk.nix
-    ./htop.nix
-    ./hyprland
-    ./kitty.nix
-    ./lazygit.nix
-    ./obs-studio.nix
-    ./nvf.nix
-    ./obs-studio.nix
-    ./rofi
-    ./qt.nix
-    ./scripts
-    ./scripts/gemini-cli.nix
-    #./starship.nix
-    #./starship-ddubs-1.nix
-    ./stylix.nix
-    ./swappy.nix
-    ./swaync.nix
-    ./tealdeer.nix
-    ./virtmanager.nix
-    waybarChoice
-    ./wlogout
-    ./xdg.nix
-    ./yazi
-    ./zoxide.nix
-    ./zsh
-    ./zen.nix
-    ./opencode.nix
-    ./editors/zed-editor
-  ]
-  ++ (if helixEnable then [ ./evil-helix.nix ] else [ ])
-  ++ (if vscodeEnable then [ ./vscode.nix ] else [ ])
-  ++ (
-    if doomEmacsEnable then
-      [
+in {
+  imports =
+    [
+      ./amfora.nix
+      ./bash.nix
+      ./bashrc-personal.nix
+      ./bat.nix
+      ./btop.nix
+      ./bottom.nix
+      ./cava.nix
+      ./emoji.nix
+      ./expressvpn.nix
+      ./eza.nix
+      ./fastfetch
+      ./fzf.nix
+      ./gh.nix
+      ./git.nix
+      ./gtk.nix
+      ./htop.nix
+      ./hyprland
+      ./kitty.nix
+      ./lazygit.nix
+      ./obs-studio.nix
+      ./nvf.nix
+      ./obs-studio.nix
+      ./rofi
+      ./qt.nix
+      ./qflipper.nix
+      ./scripts
+      ./scripts/gemini-cli.nix
+      #./starship.nix
+      #./starship-ddubs-1.nix
+      ./stylix.nix
+      ./swappy.nix
+      ./swaync.nix
+      ./tealdeer.nix
+      ./virtmanager.nix
+      waybarChoice
+      ./wlogout
+      ./xdg.nix
+      ./yazi
+      ./zoxide.nix
+      ./zsh
+      ./zen.nix
+      ./opencode.nix
+      ./editors/zed-editor
+    ]
+    ++ (
+      if helixEnable
+      then [./evil-helix.nix]
+      else []
+    )
+    ++ (
+      if vscodeEnable
+      then [./vscode.nix]
+      else []
+    )
+    ++ (
+      if doomEmacsEnable
+      then [
         ./editors/doom-emacs-install.nix
         ./editors/doom-emacs.nix
       ]
-    else
-      [ ]
-  )
-  ++ (if weztermEnable then [ ./wezterm.nix ] else [ ])
-  ++ (if ghosttyEnable then [ ./ghostty.nix ] else [ ])
-  ++ (if tmuxEnable then [ ./tmux.nix ] else [ ])
-  ++ (if alacrittyEnable then [ ./alacritty.nix ] else [ ]);
+      else []
+    )
+    ++ (
+      if weztermEnable
+      then [./wezterm.nix]
+      else []
+    )
+    ++ (
+      if ghosttyEnable
+      then [./ghostty.nix]
+      else []
+    )
+    ++ (
+      if tmuxEnable
+      then [./tmux.nix]
+      else []
+    )
+    ++ (
+      if alacrittyEnable
+      then [./alacritty.nix]
+      else []
+    );
 }
