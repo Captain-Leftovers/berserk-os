fc80c3dd624c2e2bb9ee7d2be3a693850da0d843
beeondweb <beeondweb@gmail.com>
2026-04-11 02:47:07 +1000

    adding caelestia shell to hm
    Signed-off-by: beeondweb <beeondweb@gmail.com>

--- modules/core/syncthing.nix
+++ modules/core/syncthing.nix
@@ -1,8 +1,9 @@
-{ username, ... }: {
+{username, ...}: {
   services.syncthing = {
-    enable = false;
+    enable = true;
     user = "${username}";
     dataDir = "/home/${username}";
     configDir = "/home/${username}/.config/syncthing";
+    guiAddress = "127.0.0.1:8384";
   };
 }
